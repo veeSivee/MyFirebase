35fce75237da377beb470734839b5600fbc82c53
Taufiqotul Faidah <taufiqotulfaidah@Amis-iPhone.icehousecorp.local>
2016-09-13 11:44:01 +0700

    Firebase child

--- app/src/main/java/com/tes/vi/myfirebase/MainActivity.java
+++ app/src/main/java/com/tes/vi/myfirebase/MainActivity.java
@@ -1,61 +1,69 @@
 package com.tes.vi.myfirebase;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ListViewCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.Map;
+
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView mTxtCondition;
     Button mBtnSunny, mBtnFoggy;
+    ListView mLvData;
+
     Firebase mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //init();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        init();
+    }
+
+    private void init(){
+
         mTxtCondition = (TextView)findViewById(R.id.tvCondition);
         mBtnSunny = (Button)findViewById(R.id.btnSunny);
         mBtnFoggy = (Button)findViewById(R.id.btnFoggy);
-        mRef = new Firebase("https://gate-4b0fb.firebaseio.com/condition");
+        mLvData = (ListView)findViewById(R.id.lvData);
+        mRef = new Firebase("https://gate-4b0fb.firebaseio.com");
 
-        mRef.addValueEventListener(new ValueEventListener() {
+        Firebase messageRef = mRef.child("messages");
+
+        messageRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String data = dataSnapshot.getValue(String.class);
-                mTxtCondition.setText(data);
+                Map<String,String> map = dataSnapshot.getValue(Map.class);
+                String one = map.get("one");
+                String two = map.get("two");
+                String three = map.get("three");
+                mTxtCondition.setText(one + " ~ " + two + " ~ " + three);
             }
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-                mTxtCondition.setText("!!! " + firebaseError.toString());
-            }
-        });
 
-        mBtnSunny.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mRef.setValue("Sunny Gooo");
-            }
-        });
-
-        mBtnFoggy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mRef.setValue("F for foggy");
             }
         });
     }
